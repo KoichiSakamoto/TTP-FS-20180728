ecc228c988fef8beade58fd5be0849df7a782030
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-11-25 18:36:03 -0500

    updated database to be postgresql not sqlite3 for heroku deployment

--- nytt-backend/app/serializers/portfolio_serializer.rb
+++ nytt-backend/app/serializers/portfolio_serializer.rb
@@ -1,5 +1,5 @@
 class PortfolioSerializer < ActiveModel::Serializer
-attributes :id, :user_id
-belongs_to :user
-has_many :stocks
+  attributes :id, :user_id
+  belongs_to :user
+  has_many :stocks
 end
