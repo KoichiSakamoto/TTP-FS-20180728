dcf489bc7df348cb53ccd6cf448f64eceb05b213
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-11-26 00:23:25 -0500

    implemented React-Router, created app urls

--- nytt-frontend/src/App.js
+++ nytt-frontend/src/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom'
-
+import PortfolioPage from './Containers/PortfolioPage.js'
+import TransactionsPage from './Containers/TransactionsPage.js'
+import WelcomePage from './Containers/WelcomePage.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route path="" component={WelcomePage} exact/>
+          <Route path="/portfolio" component={PortfolioPage}/>
+          <Route path="/transactions" component={TransactionsPage}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
