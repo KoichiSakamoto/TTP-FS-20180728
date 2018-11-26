b614c654ededb1acf3bb79cf3e03a570f1919668
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-11-25 20:19:37 -0500

    added seed data for testing during development

--- nytt-backend/db/seeds.rb
+++ nytt-backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create({username: 'Isaac Fiore', email: "isaacfiore@butts.com"})
+
+portfolio1 = Portfolio.create({user_id: 1})
+
+stock1 = Stock.create({tickername: "YAS", portfolio_id: 1, quantity: 5000})
+
+transaction1 = Transaction.create({user_id: 1, stock_id: 1, purchase_or_sale: "purchase", quantity: 5000})
