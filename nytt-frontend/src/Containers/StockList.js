77baff6903728d9f9d94b48e4fbdce4650798977
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-11-26 14:03:05 -0500

    finished displaying user's stocks in portfolio

--- nytt-frontend/src/Containers/StockList.js
+++ nytt-frontend/src/Containers/StockList.js
@@ -3,8 +3,25 @@ import StockRow from '../Components/StockRow.js'
 
 class StockList extends Component {
 
+  state = {
+    userStocks: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:4000/api/v1/users/1')
+    .then(res => res.json())
+    .then(json => this.setState({
+      userStocks: json.stocks
+    }))
+  }
+
   displayStocks = () => {
-    return <StockRow stock={{tickername: "AYY", quantity: 500}} />
+    //console.log(this.state.userStocks)
+    if (this.state.userStocks.length === 0)  {
+      return null
+    } else {
+      return this.state.userStocks.map((stock) => <StockRow key={stock.id} stock={stock} />)
+    }
   }
 
   render() {
